aaa91cd16471aa45df314ecb5c4db190f68efaf0
Gayathri <vadladigayathri@gmail.com>
2026-05-22 07:44:26 +0530

    added screenshots

--- src/main/java/pages/LoginPageTest.java
+++ src/main/java/pages/LoginPageTest.java
@@ -14,7 +14,7 @@ public class LoginPageTest {
 
     }
 
-    By loginLink = By.xpath("//a[contains(@title,'Jobseeker Login')]");
+    By loginLink = By.xpath("//a[contains(@title,'Login')]");
     By email = By.id("usernameField");
     By password = By.id("passwordField");
     By loginButton = By.xpath("//button[@type=\"submit\"]");
