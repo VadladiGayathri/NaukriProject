4ab61ee665b369be224af6cc365ead17801d15e4
Gayathri <vadladigayathri@gmail.com>
2026-05-22 07:10:34 +0530

    Fixed in Chrome driver version

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -3,6 +3,7 @@ package base;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BaseTest {
 
@@ -11,7 +12,11 @@ public class BaseTest {
 
 
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--start-maximized");
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.get("https://www.naukri.com/nlogin/login?URL=https://www.naukri.com/mnjuser/homepage");
 
